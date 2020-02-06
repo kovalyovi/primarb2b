3fe1be2202e32cb09881eeb18e01a535cd38d7b2
Ilya Kovalyov <ilyakov@byui.edu>
2020-02-06 15:49:11 -0700

    Add project files.

---
+++ CKTPortal/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CKTPortal.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Tag { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
